308c3372cd489a126a61e0fe05b3c10ca7e66c0a
Andreia Ferreira <aluno19985@ipt.pt>
2019-07-22 15:05:54 +0100

    inserção de migrations

---
+++ migrations/20190722135951-Locations.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    
+   return queryInterface.createTable('Locations', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true 
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+      
+    });
+
+    
+  },
+
+  down: (queryInterface) => {
+    
+   return queryInterface.dropTable('Locations');
+  }
+};
